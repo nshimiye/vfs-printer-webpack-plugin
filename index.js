1ec8dc2ddfccbd4223bdc787c6a16316cd1321ac
Marcellin Nshimiyimana <nshimiye.tech@gmail.com>
2021-07-31 09:52:17 -0400

    fix path creation

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { normalize, parse } = require('path');
 class VFSPrinter {
     pluginName = 'vfs-printer'
     constructor(options) {this.options = options; }
@@ -5,13 +6,26 @@ class VFSPrinter {
 
         compiler.hooks.beforeRun.tapAsync('VfsToRfs', ({ inputFileSystem }, callback) => {
             inputFileSystem._webpackCompilerHost._memoryHost.write = function (path, content) {
-            const filePath = `vfs/${path.replace(process.cwd(), '')}`;
-            const [parentFolder, /* fileName */] = filePath.split(/([^\/]+)$/);
-            require('fs').mkdirSync(
-                parentFolder, { recursive: true }
-            );
-            const td = new TextDecoder();
-            require('fs').writeFileSync(filePath, td.decode(content));
+
+                // START get virtual path components
+                const rootPath = normalize(process.cwd());
+                const rootFolder = parse(rootPath).base;
+                const relativePath = path.split(rootFolder)[1];
+                
+                const fileNameWithExt = parse(normalize(path)).base;
+                const relativeParentPath = relativePath.replace(fileNameWithExt, '');
+                
+                const filePath = `vfs${relativePath}`;
+                const parentFolder = `vfs${relativeParentPath}`;
+                // END get virtual path components
+
+                // create folder structure if not done already
+                require('fs').mkdirSync(
+                    parentFolder, { recursive: true }
+                );
+
+                const td = new TextDecoder();
+                require('fs').writeFileSync(filePath, td.decode(content));
                 return this._doSyncCall(this._delegate.write(path, content));
             }
             callback();
