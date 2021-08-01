0965b83f2b35aeb3b4ea9b5190ecc123f51377a2
Marcellin Nshimiyimana <nshimiye.tech@gmail.com>
2021-08-01 17:10:21 -0400

    add option to print modules

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const { normalize, parse } = require('path');
 class VFSPrinter {
     pluginName = 'vfs-printer'
     constructor(options) {this.options = options; }
+    vfsFileMap = {};
+    vfsFileNames = new Set();
     apply(compiler) {
+        const _this = this;
 
         compiler.hooks.beforeRun.tapAsync('VfsToRfs', ({ inputFileSystem }, callback) => {
             inputFileSystem._webpackCompilerHost._memoryHost.write = function (path, content) {
@@ -19,6 +22,12 @@ class VFSPrinter {
                 const parentFolder = `vfs${relativeParentPath}`;
                 // END get virtual path components
 
+                if(fileNameWithExt.endsWith('js')) {
+                    const fileName = parse(normalize(path)).name;
+                    _this.vfsFileNames.add(fileName);
+                    _this.vfsFileMap[Buffer.from(fileName).toString('base64')] = parentFolder+fileName+'.wpm.js';
+                }
+
                 // create folder structure if not done already
                 require('fs').mkdirSync(
                     parentFolder, { recursive: true }
@@ -30,6 +39,27 @@ class VFSPrinter {
             }
             callback();
         });
+
+        
+        /**
+         * print modules that are derived from virtual files 
+         */
+        compiler.hooks.thisCompilation.tap(this.pluginName, (compilation, compilationParams) => {
+            compilation.hooks.finishModules.tapAsync(this.pluginName, (modules, callback) => {
+                const files = Array.from(_this.vfsFileNames);
+                modules.forEach(m => {
+                    if(m.rawRequest) {
+                        const fn = files.find(fileName => m.rawRequest.includes(fileName)); 
+                        if(fn) {
+                            const fp = _this.vfsFileMap[Buffer.from(fn).toString('base64')];
+                            require('fs').writeFileSync(fp, m._source._value);
+                        }
+                    }
+                });
+                callback();
+            });
+        });
+
 
     }
 }
